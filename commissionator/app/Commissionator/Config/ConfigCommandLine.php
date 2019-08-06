4ac430773a42160dd8212a47150cddca93ac483b
Jed Lagunday <jed.lagunday@gmail.com>
2019-08-06 19:49:27 +0800

    Revert "The commissionator"
    This reverts commit 99e7ec01558b9ea77394415a865f6de1bf28f66a.

--- commissionator/app/Commissionator/Config/ConfigCommandLine.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Commissionator\Config;
-
-class ConfigCommandLine
-{
-	const PARAMS_COUNT = '2';
-	const FILE_EXTENSION = '.csv';
-}
--- commissionator/script.php
+++ commissionator/script.php
@@ -1,12 +1,1 @@
 <?php
-require_once 'app/start.php';
-
-use Commissionator\Controllers\ParseCommandLine;
-use Commissionator\Controllers\ParseInput;
-use Commissionator\Controllers\PrintOutput;
-
-$cmd = new ParseCommandLine($argv);
-$input = new ParseInput();
-$rows = $input -> parseCsv($cmd->getArgv());
-$output = new PrintOutput();
-$output->printOutput($rows);
